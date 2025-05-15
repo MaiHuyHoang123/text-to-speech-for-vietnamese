0e7f5bb159a8306c4b67e7fe0fbb361865dde31e
Mai Huy Hoang <adamshopevent123@gmail.com>
2025-05-15 22:30:23 +0700

    test:

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import onnxruntime as ort
 import numpy as np
 import soundfile as sf
 import io
+import os
 
 app = Flask(__name__)
-session = ort.InferenceSession("./finetuning_pretrained_vi_female_epoch=4785.onnx")  # Thay bằng đường dẫn tới mô hình ONNX của bạn
+# sess_options = onnxruntime.SessionOptions()
+# session = ort.InferenceSession("./finetuning_pretrained_vi_female_epoch=4785.onnx",sess_options)  # Thay bằng đường dẫn tới mô hình ONNX của bạn
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         text = request.form['text']
-        inputs = {"text": text}  # Điều chỉnh đầu vào theo mô hình của bạn
-        outputs = session.run(None, inputs)
-        waveform = outputs[0]  # Giả định đầu ra là dạng sóng âm thanh
-        buffer = io.BytesIO()
-        sf.write(buffer, waveform, samplerate=22050, format='WAV')
-        buffer.seek(0)
-        return send_file(buffer, mimetype='audio/wav')
+        # inputs = {"input": text,"scales": 0.67,"input_lengths": len(text)}  # Điều chỉnh đầu vào theo mô hình của bạn
+        # outputs = session.run(None, inputs)
+        # waveform = outputs[0]  # Giả định đầu ra là dạng sóng âm thanh
+        # buffer = io.BytesIO()
+        # sf.write(buffer, waveform, samplerate=22050, format='WAV')
+        # buffer.seek(0)
+        os.system("""echo {text} | \
+  piper -m ./finetuning_pretrained_vi_female_epoch=4785.onnx --output_file ./test_v2.wav""")
+        return "test"
     return render_template_string('''
         <!DOCTYPE html>
         <html lang="vi">
